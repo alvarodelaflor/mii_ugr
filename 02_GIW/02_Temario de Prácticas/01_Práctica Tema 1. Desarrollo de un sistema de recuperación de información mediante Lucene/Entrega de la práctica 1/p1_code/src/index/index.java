59c1f2eff9efaaf1a014d3de7f1e50c5322421c8
Álvaro de la Flor Bonilla <info@alvarodelaflor.com>
2021-03-16 16:17:43 +0100

    GIW P1 Initial

---
+++ 02_GIW/02_Temario de Prácticas/01_Práctica Tema 1. Desarrollo de un sistema de recuperación de información mediante Lucene/Entrega de la práctica 1/p1_code/src/index/index.java
@@ -0,0 +1,63 @@
+package index;
+
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.CorruptIndexException;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Paths;
+
+public class index {
+
+    private IndexWriter writer;
+    public void Indexer(String indexDirectoryPath) throws IOException {
+        Directory indexDirectory =
+                FSDirectory.open(Paths.get(indexDirectoryPath));
+        StandardAnalyzer analyzer = new StandardAnalyzer();
+        IndexWriterConfig iwc = new IndexWriterConfig(analyzer);
+        writer = new IndexWriter(indexDirectory, iwc);
+    }
+    public void close() throws CorruptIndexException, IOException {
+        writer.close();
+    }
+    private Document getDocument(File file) throws IOException {
+        Document document = new Document();
+        TextField contentField = new TextField("contents", new FileReader(file));
+        TextField fileNameField = new TextField("filename",
+                file.getName(),TextField.Store.YES);
+        TextField filePathField = new TextField("filepath",
+                file.getCanonicalPath(),TextField.Store.YES);
+        document.add(contentField);
+        document.add(fileNameField);
+        document.add(filePathField);
+        return document;
+    }
+    private void indexFile(File file) throws IOException {
+        System.out.println("Indexing "+file.getCanonicalPath());
+        Document document = getDocument(file);
+        writer.addDocument(document);
+    }
+    public int createIndex(String dataDirPath, FileFilter filter)
+            throws IOException {
+        File[] files = new File(dataDirPath).listFiles();
+        for (File file : files) {
+            if(!file.isDirectory()
+                    && !file.isHidden()
+                    && file.exists()
+                    && file.canRead()
+                    && filter.accept(file)
+            ){
+                indexFile(file);
+            }
+        }
+        return writer.numRamDocs();
+    }
+}
