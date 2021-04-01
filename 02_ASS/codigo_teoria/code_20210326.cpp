93588cdea173fb69a7a2a7530f1b19571b1c4ce6
Alvaro de la Flor Bonilla <alvarodelaflor@gmail.com>
2021-04-01 13:07:39 +0200

    ASS Ultimo teoria

---
+++ 02_ASS/codigo_teoria/code_20210326.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+int main()
+{
+  int autenticacion = 0;
+  char cUsuario[ 10 ];
+  char cPassword[ 10 ];
+  cout << "Usuario: ";
+  cin >> cUsuario;
+  cout << "Password: ";
+  cin >> cPassword;
+  
+  if( strcmp( cUsuario, "root" ) == 0 && strcmp( cPassword, "rootpass" ) == 0 )
+    autenticacion = 1;
+  if( autenticacion )
+    cout << "Acceso obtenido" << endl;
+  else
+    cout << "Nombre de usuario y/o password incorrectos" << endl;
+  return 0;
+}
