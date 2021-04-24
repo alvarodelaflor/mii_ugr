931289f1711fa5bdb61a8455989304558f982145
Alvaro de la Flor Bonilla <alvarodelaflor@gmail.com>
2021-04-24 11:18:11 +0200

    EV Cubo con gdscript reducido

--- 02_EV/01_actividad_evaluable/MultiMeshInstance.gd
+++ 02_EV/01_actividad_evaluable/MultiMeshInstance.gd
@@ -1,15 +1,7 @@
 tool
 extends MultiMeshInstance
 
-export var cube_per_side = 16 setget set_cube_per_side
-
-func set_cube_per_side(new_cubes_per_side):
-	cube_per_side = new_cubes_per_side
-	update()
-	
-func update():
-	setup_mutimesh()
-	generate_sphere()
+export var cube_per_side = 16
 	
 func setup_mutimesh():
 	self.multimesh = MultiMesh.new()
@@ -18,20 +10,18 @@ func setup_mutimesh():
 	self.multimesh.instance_count = cube_per_side * cube_per_side * cube_per_side
 	self.multimesh.visible_instance_count = self.multimesh.instance_count
 	
-	var mesh = CubeMesh
+	var mesh = CubeMesh.new()
 	mesh.size = Vector3(1,1,1)
 	self.multimesh.mesh = mesh
 	
-func generate_sphere():
 	var cube_size = 1
 	var cube_half_size = cube_size / 2.0
 	var origin_distance = cube_per_side * cube_half_size - cube_half_size
 	var origin = -Vector3(origin_distance, origin_distance, origin_distance)
 	var index = 0
 	
-	for x in range(cube_per_side):
-		for y in range(cube_per_side):
-			for z in range(cube_per_side):
-				var pos = origin * Vector3(x * cube_size, y * cube_size, z * cube_size)
-				self.multimesh.set_instance_transform(index, Transform(Basis(), pos))
-				index += 1
+	var pos = origin * Vector3(0, 0, 0)
+	self.multimesh.set_instance_transform(index, Transform(Basis(), pos))
+	
+func _ready():
+	setup_mutimesh()
