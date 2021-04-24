35f04a60a6c1107404590da3387a6bdfc2ee6b24
Álvaro de la Flor Bonilla <info@alvarodelaflor.com>
2021-04-24 11:56:15 +0200

    EV Nombre variable

--- 02_EV/01_actividad_evaluable/MultiMeshInstance.gd
+++ 02_EV/01_actividad_evaluable/MultiMeshInstance.gd
@@ -1,26 +1,26 @@
 tool
 extends MultiMeshInstance
 
-export var cube_per_side = 2 setget reload_scene
+export var cube_size = 2 setget reload_scene
 
-func reload_scene(size_cube):
-	cube_per_side = size_cube
+func reload_scene(new_size_cube):
+	cube_size = new_size_cube
 	setup_mutimesh()
 	
 func setup_mutimesh():
 	self.multimesh = MultiMesh.new()
 	self.multimesh.transform_format = MultiMesh.TRANSFORM_3D
 	
-	self.multimesh.instance_count = cube_per_side * cube_per_side * cube_per_side
+	self.multimesh.instance_count = cube_size * cube_size * cube_size
 	self.multimesh.visible_instance_count = self.multimesh.instance_count
 	
 	var mesh = CubeMesh.new()
-	mesh.size = Vector3(cube_per_side,cube_per_side, cube_per_side)
+	mesh.size = Vector3(cube_size,cube_size, cube_size)
 	self.multimesh.mesh = mesh
 	
 	var cube_size = 1
 	var cube_half_size = cube_size / 2.0
-	var origin_distance = cube_per_side * cube_half_size - cube_half_size
+	var origin_distance = cube_size * cube_half_size - cube_half_size
 	var origin = -Vector3(origin_distance, origin_distance, origin_distance)
 	var index = 0
 	
