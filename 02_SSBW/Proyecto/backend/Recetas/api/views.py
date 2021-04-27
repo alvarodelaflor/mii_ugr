dd83a051adee64913fa2de81a51920eea049b362
Alvaro de la Flor Bonilla <alvarodelaflor@gmail.com>
2021-04-27 22:15:54 +0200

    SSWB Models

--- 02_SSBW/Proyecto/backend/Recetas/api/views.py
+++ 02_SSBW/Proyecto/backend/Recetas/api/views.py
@@ -1,9 +1,38 @@
 from django.shortcuts import render
-from django.conf.urls import url
-from . import views
 
-urlpatterns = [
-    url(r'^api/recetas$', views.receta_list),
-    url(r'^api/recetas/(?P<pk>[0-9]+)$', views.receta_detail),
-    url(r'^api/recetas/published$', views.receta_list_published)
-]
+from django.http.response import JsonResponse
+from rest_framework.parsers import JSONParser
+from rest_framework import status
+
+from .models import Receta
+from .serializers import RecetaSerializer
+from rest_framework.decorators import api_view
+
+
+@api_view(['GET', 'POST', 'DELETE'])
+def tutorial_list(request):
+    if request.method == 'GET':
+        tutorials = Receta.objects.all()
+
+        title = request.GET.get('title', None)
+        if title is not None:
+            tutorials = tutorials.filter(title__icontains=title)
+
+        tutorials_serializer = RecetaSerializer(tutorials, many=True)
+        return JsonResponse(tutorials_serializer.data, safe=False)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def tutorial_detail(request, pk):
+    # find tutorial by pk (id)
+    try:
+        receta = Receta.objects.get(pk=pk)
+    except Receta.DoesNotExist:
+        return JsonResponse({'message': 'The tutorial does not exist'}, status=status.HTTP_404_NOT_FOUND)
+
+        # GET / PUT / DELETE tutorial
+
+
+@api_view(['GET'])
+def tutorial_list_published(request):
+# GET all published tutorials
