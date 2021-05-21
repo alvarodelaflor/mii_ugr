89885fe0c111881a1d8fb22ca6a788bf4f716a89
Alvaro de la Flor Bonilla <alvarodelaflor@gmail.com>
2021-05-21 13:50:02 +0200

    EV Animaciones

--- 02_EV/06_Practicas/03_Práctica 3 Introducción de cámaras, iluminación y materiales/p3_final/light_animation.gd
+++ 02_EV/06_Practicas/03_Práctica 3 Introducción de cámaras, iluminación y materiales/p3_final/light_animation.gd
@@ -6,3 +6,7 @@ func _process(_delta):
 func get_input_keyboard():
 	if (Input.is_action_pressed("l")):
 		get_node("AnimationPlayer").play("light")
+	elif (Input.is_action_pressed("c")):
+		get_node("AnimationPlayer2").play("camara")
+	elif (Input.is_action_pressed("r")):
+		get_node("AnimationPlayer3").play("run")
