1243f89b2fd5a7fa59586dc781c9c54988585d96
Alvaro de la Flor Bonilla <alvarodelaflor@gmail.com>
2021-06-20 22:26:48 +0200

    SSBW Preparacion del directorio

--- 02_SSBW/Proyecto/backend/Recetas/Recetas/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from django.conf.urls import url, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    url(r'^', include('api.urls')),
-]
--- 02_SSBW/Proyecto/backend/Recetas/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^api/recetas$', views.receta_list),
-    url(r'^api/recetas/(?P<pk>[0-9]+)$', views.receta_detail),
-    url(r'^api/recetas/published$', views.receta_list_published)
-]
