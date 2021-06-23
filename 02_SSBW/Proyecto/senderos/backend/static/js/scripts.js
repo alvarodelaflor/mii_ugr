f5c9e84ab5deda9d561cb8a3fd0a9f32252d9458
Alvaro de la Flor Bonilla <alvarodelaflor@gmail.com>
2021-06-23 20:13:03 +0200

    SSBW Ajax

--- 02_SSBW/Proyecto/senderos/backend/static/js/scripts.js
+++ 02_SSBW/Proyecto/senderos/backend/static/js/scripts.js
@@ -1,7 +1,37 @@
-/*!
-* Start Bootstrap - Shop Homepage v5.0.1 (https://startbootstrap.com/template/shop-homepage)
-* Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-homepage/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+let liketotal = document.getElementById('num_likes');
+let likes = Number(liketotal.innerText);
+const btnDislike = document.getElementById('btn_Dislike')
+const btnLike = document.getElementById('btn_Like')
+var myVar = document.getElementById("myVar").value;
+
+btnDislike.addEventListener('click', ()=>{
+    likes -= 1;
+    //liketotal.innerHTML = likes; // Tarea 7
+    send_value(likes); // Tarea 8
+})
+
+btnLike.addEventListener('click', ()=>{
+    likes += 1;
+    //liketotal.innerHTML = likes; // Tarea 7
+    send_value(likes); // Tarea 8
+})
+
+// realizamos la modificación de los likes usando el API
+function send_value(value) {
+    $.ajax({
+        'url': myVar, // el end-point
+        'type': 'PUT',
+        'dataType': 'json', // Content-Type de vuelta
+        'data': JSON.stringify({
+            'likes': value
+        }), // el api espera un string json
+        'success': function (data) { // call-back, se ejecuta con el response
+            //console.log(data + " success send");
+            liketotal.innerHTML = likes;
+        },
+        error: function (result) {
+            alert(JSON.stringify(result))
+            //console.log(result.responseJSON + " error send");
+        }
+    })
+}
