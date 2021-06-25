28ee5171f5b8c7e15866a72613f4a0b494081f52
Alvaro de la Flor Bonilla <alvarodelaflor@gmail.com>
2021-06-25 21:54:51 +0200

    SSWB React

--- 02_SSBW/Proyecto/senderos/frontend/src/App.js
+++ 02_SSBW/Proyecto/senderos/frontend/src/App.js
@@ -7,6 +7,7 @@ class App extends Component {
 		super(props)
 		this.state = {
 			items: [],
+			show: [],
 			isLoaded: false,
 		}
 	}
@@ -24,6 +25,7 @@ class App extends Component {
 				this.setState({
 					isLoaded: true,
 					items: json,
+					show: json,
 				})
 			})
 			.catch(error => {
@@ -31,20 +33,42 @@ class App extends Component {
 			})
 	}
 
-	render() {
+	handleSearch(event) {
 
 		var {isLoaded, items} = this.state;
+		var res = items
+
+		var searchQuery = event.target.value.toLowerCase();
+		res = items.filter(item => item.nombre.toLowerCase().includes(searchQuery));
+		this.setState({
+					isLoaded: true,
+					items: items,
+					show: res,
+		})
+  	}
+
+	render() {
+
+		var {isLoaded, items, show} = this.state;
 
 		if (!isLoaded) {
 			return <div>Loading ...</div>
 		} else {
 			return (
 				<div className="App">
+					<div className="container px-4 px-lg-5 my-5">
+						<div className="input-group">
+							<input type="search" className="form-control rounded"
+								   placeholder="Introduzca el término de búsqueda" aria-label="Search"
+								   aria-describedby="search-addon" onChange={this.handleSearch.bind(this)}/>
+							<button type="button" className="btn btn-outline-primary">search</button>
+						</div>
+					</div>
 					<section className="py-5">
 						<div className="container px-4 px-lg-5 mt-5">
 							<div
 								className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
-								{items.map(item => (
+								{show.map(item => (
 									<div className="col mb-5">
 										<div className="card h-100">
 											<img className="card-img-top" src={item.foto}
@@ -52,14 +76,6 @@ class App extends Component {
 											<div className="card-body p-4">
 												<div className="text-center">
 													<h5 className="fw-bolder">{item.nombre}</h5>
-													<div className="d-flex justify-content-center small text-warning mb-2">
-														<div className="bi-star-fill"></div>
-														<div className="bi-star-fill"></div>
-														<div className="bi-star-fill"></div>
-														<div className="bi-star-fill"></div>
-														<div className="bi-star-fill"></div>
-													</div>
-													<span className="text-muted">{item.owner}:</span>
 													<span>{item.descripcion}</span>
 													<br></br>
 												</div>
