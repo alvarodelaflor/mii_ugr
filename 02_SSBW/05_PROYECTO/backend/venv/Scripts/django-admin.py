54ff7cb785053e5832632533f4d3feaba370917f
Alvaro de la Flor Bonilla <alvarodelaflor@gmail.com>
2021-07-18 14:04:21 +0200

    SSBW Final

---
+++ 02_SSBW/05_PROYECTO/backend/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Alvaro\Documents\mii_ugr\02_SSBW\Proyecto\backend\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
